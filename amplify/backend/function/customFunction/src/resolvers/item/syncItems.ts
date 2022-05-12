496e21e8b38a232151d7f33093f66e352561e9c8
Matt Davies <matthew.davies@sovtech.com>
2022-05-12 23:12:25 +0200

    refactor(custom-function): remove test logs from sync items resolveer

--- amplify/backend/function/customFunction/src/resolvers/item/syncItems.ts
+++ amplify/backend/function/customFunction/src/resolvers/item/syncItems.ts
@@ -8,24 +8,19 @@ export default async (event, context) => {
    *
    */
   const { items, groupId } = event.arguments
-  const identity = event.identity
+  const username = event.identity.sub
   if (items && items.length > 0) {
     try {
       const group = await get("Group", groupId)
-      console.log("Group", group)
       await Promise.all([
         // map through each updated item
         ...items.map(async (updatedItem) => {
-          console.log("Updated Item", updatedItem)
           // get current record of that udpated item
           const item = await get("Item", updatedItem.id)
-          console.log("Item", item)
           // initialise a variable that will store fields and values to be updated
           let input = {}
           // map through each updated item's fields and compare them to the current record's values of those fields
           Object.keys(updatedItem).forEach((field) => {
-            console.log(`Updated Item - ${field}:`, updatedItem[field])
-            console.log(`Item - ${field}:`, item[field])
             if (updatedItem[field] !== item[field]) {
               // field has been updated
               if (
@@ -36,18 +31,17 @@ export default async (event, context) => {
                  *  The item was marked complete
                  *  record user identity who envoked this mutation
                  */
-                input["completedById"] = identity
+                input["completedById"] = username
               }
               input[field] = updatedItem[field]
             }
           })
-          console.log("Input", input)
           return update("Item", updatedItem.id, input)
         }),
       ])
       return "Successfully Synced Items!"
     } catch (err) {
-      console.log("Error", err)
+      console.log("Error Syncing Items", err)
       throw new Error("Error Syncing Items")
     }
   } else {
